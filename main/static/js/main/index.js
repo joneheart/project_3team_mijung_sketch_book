bbf028f70c012c2b6605c9a5f24408febd4a9c66
jungjaechoi <joong8812@gmail.com>
2022-02-23 19:25:00 +0900

    add - (main) 나의작업실, 나의유화 버튼 추가

---
+++ main/static/js/main/index.js
@@ -0,0 +1,31 @@
+
+const mouseEnterMyBtn = (e) => {
+    const curClassName = e.target.classList[1];
+    const imgSrc = `/static/images/my-${curClassName}-activate.png`
+    e.target.setAttribute('src', imgSrc);
+}
+
+const mouseLeaveMyBtn = (e) => {
+    const curClassName = e.target.classList[1];
+    const imgSrc = `/static/images/my-${curClassName}-normal.png`
+    e.target.setAttribute('src', imgSrc);
+}
+
+const mouseClickMyBtn = (e) => {
+    const curClassName = e.target.classList[1];
+    const isClickedMyPaintings = curClassName === "paintings" ? true : false;
+    if (isClickedMyPaintings) {
+        location.href = "#" // go to my paintings
+    } else {
+        location.href = "#" // go to my workspace
+    }
+}
+
+// 실행부
+myButtons = document.querySelectorAll('.btn-my');
+
+for (let i=0; i<myButtons.length; i++) {
+    myButtons[i].addEventListener('mouseenter', mouseEnterMyBtn);
+    myButtons[i].addEventListener('mouseleave', mouseLeaveMyBtn);
+    myButtons[i].addEventListener('click', mouseClickMyBtn);
+}
