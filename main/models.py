2bf92f0c86020a28eb1f36f7087834df340cbefe
joneheart <tladlf33@naver.com>
2022-02-23 18:35:04 +0900

    추가 - main앱 db 테이블 추가

--- main/models.py
+++ main/models.py
@@ -3,3 +3,11 @@ from django.db import models
 
 # Create your models here.
 
+class BasePicture(models.Model):
+    class Meta:
+        db_table = "painting"
+
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=50, null=True)
+    year = models.CharField(max_length=10)
+    intro = models.TextField()
