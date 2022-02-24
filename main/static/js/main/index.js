d4e68e5bed67406559b3e8b6a6c93db7a9675eb5
jungjaechoi <joong8812@gmail.com>
2022-02-24 20:41:01 +0900

    add - [메인] 이미지 관련 액션 처리(선택, 취소, 이펙트, css)

--- main/static/js/main/index.js
+++ main/static/js/main/index.js
@@ -21,11 +21,112 @@ const mouseClickMyBtn = (e) => {
     }
 }
 
-// 실행부
-myButtons = document.querySelectorAll('.btn-my');
+const getImgElement = (imgSrc) => {
+    const imgTag = document.createElement('img')
+    imgTag.setAttribute('src', imgSrc);    
+    return imgTag;
+}
 
+const showPaintingInfo = (e) => {
+    if (selectedPainting != null) {
+        releaseAllPaintings();
+        return;
+    }
+
+    clickedPaintingElement = e.target;
+    const famousPaintingSrc = e.target.currentSrc;
+    const modalPopup = document.querySelector('.modal-popup');
+    modalPopup.appendChild(getImgElement(famousPaintingSrc));
+
+    // temp data
+    const tempData = ["Mona Lisa", "1503", "Leonardo Da Vinci"]
+    const paintingsAllInfo = document.querySelectorAll('.paintings-info h4');
+    paintingsAllInfo.forEach((eachInfo, i) => {
+        eachInfo.innerText = tempData[i];
+    })
+
+    const imgHeight = modalPopup.children[1].height; // 모달 속 명화 이미지 높이
+    const paintingsInfoDiv = document.querySelector('.paintings-info'); // 명화 정보 div select
+    const infoHeight = paintingsInfoDiv.offsetHeight;
+    paintingsInfoDiv.style.top = `${imgHeight - infoHeight}px`;
+
+    document.querySelector('.modal-background').classList.toggle("show");
+}
+
+const closeModal = (modalWindow) => {
+    const imgElementInModal = modalWindow.children[0].children[1];
+    document.querySelector('.modal-background').classList.toggle("show");
+    document.querySelector('.modal-popup').removeChild(imgElementInModal);
+}
+
+const clickModalWindow = (e) => {
+    const clickedClassName = e.target.className;
+    
+    if (clickedClassName == "modal-window") {
+        closeModal(e.target);
+    }
+}
+
+const selectPainting = (e) => {
+    clickedPaintingElement.classList.add('selected');
+    const allPaintings = document.querySelectorAll('.famous-paintings');
+    allPaintings.forEach(element => {
+        if (element != clickedPaintingElement) {
+            element.classList.add('disabled');
+        }
+    });
+    selectedPainting = clickedPaintingElement;
+    closeModal(document.querySelector('.modal-window'));
+}
+
+const selectCancel = (e) => {
+    closeModal(document.querySelector('.modal-window'));
+}
+
+const isSelectedPainting = () => {
+    const allPaintings = document.querySelectorAll('.famous-paintings');
+    const amountsOfClass = allPaintings[0].classList.length + allPaintings[1].classList.length;
+    return amountsOfClass != 2 ? true : false;
+}
+
+const releaseAllPaintings = () => {
+    const allPaintings = document.querySelectorAll('.famous-paintings');
+    allPaintings.forEach(element => {
+        element.classList.remove('disabled');
+    });
+    selectedPainting.classList.remove('selected');
+    selectedPainting = null;
+}
+
+const escCancel = (e) => {
+    if (e.keyCode == "27" && isSelectedPainting()) {
+        releaseAllPaintings();
+    } else {
+        const cntModalBackClass = document.querySelector('.modal-background').classList.length;
+        if (cntModalBackClass == 2) {
+            closeModal(document.querySelector('.modal-window'));
+        }
+    }
+}
+
+// 실행부
+let clickedPaintingElement = null;
+let selectedPainting = null;
+
+const myButtons = document.querySelectorAll('.btn-my');
 for (let i=0; i<myButtons.length; i++) {
     myButtons[i].addEventListener('mouseenter', mouseEnterMyBtn);
     myButtons[i].addEventListener('mouseleave', mouseLeaveMyBtn);
     myButtons[i].addEventListener('click', mouseClickMyBtn);
 }
+
+const famousPaintings = document.querySelectorAll('.famous-paintings');
+for (let i=0; i<famousPaintings.length; i++) {
+    famousPaintings[i].addEventListener('click', showPaintingInfo);
+}
+
+document.querySelector('.modal-window').addEventListener('click', clickModalWindow);
+document.querySelector('#select-painting').addEventListener('click', selectPainting);
+document.querySelector('#select-cancel').addEventListener('click', selectCancel);
+
+window.addEventListener('keydown', escCancel);
