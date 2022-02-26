4aa33867231046971a4a04975a02f2c8b8fcb17d
jungjaechoi <joong8812@gmail.com>
2022-02-26 10:44:22 +0900

    change - [나의유화] 페이지 접속시 로그아웃 버튼 보인 후 사라지는 효과

--- user/static/js/user/mypage.js
+++ user/static/js/user/mypage.js
@@ -1,7 +1,6 @@
 
 const enterLogout = (e) => {
     e.target.style.opacity = '1';
-    e.target.style.transition = 'opacity 2s';
 }
 
 const leaveLogout = (e) => {
@@ -20,3 +19,8 @@ document.querySelector('#img-logout').addEventListener('mouseover', enterLogout)
 document.querySelector('#img-logout').addEventListener('mouseleave', leaveLogout);
 document.querySelector('#img-logout').addEventListener('click', logout);
 document.querySelector('#back-arrow-wrapper > img').addEventListener('click', goToMain);
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('#img-logout').style.opacity = '0';
+    document.querySelector('#img-logout').style.transition = 'opacity 1.5s';
+});
