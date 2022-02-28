eb64b364b54f7ec9643823beaf0830c8cea4cbe9
nxxxtyetdecided <nxxxtyetdecided@gmail.com>
2022-02-28 09:23:57 +0900

    로그인 url 수정

--- user/urls.py
+++ user/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.login),
     path('sign-up/', views.sign_up, name='sign-up'),
-    path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
 ]
